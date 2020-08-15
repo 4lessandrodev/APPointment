f4fc293acd36d1b503041921bc2f5a3f654a34ff
Alessandro <alessandroadm@live.com>
2020-08-15 02:50:31 -0300

    feat(controllers): adding on index register method

--- APPointment/controller/index.js
+++ APPointment/controller/index.js
@@ -1,4 +1,5 @@
 const { User } = require('./../models');
+const bcrypt = require('bcrypt');
 
 module.exports = {
 
@@ -8,7 +9,29 @@ module.exports = {
             res.status(200).json({ users });   
         } catch (error) {
             console.error(error);
+            return res.status(501).json({ error:error.message });
         }
+    },
+
+    register: async (req, res) => {
+        try {
+            const { name, email, password } = req.body;
+            const userAlreadyExist = await User.findOne({ where: { email } });
+            if (userAlreadyExist) {
+                return res.status(401).json({ error: true, messages: [{ text: 'Este email já está cadastrado' }] });
+            }
+            const encryptedPassword = bcrypt.hashSync(password, 10);
+            const result = await User.create({ name, email, password: encryptedPassword });
+            result.password = undefined;
+            return res.status(200).json({ result });
+        } catch (error) {
+            console.error(error);
+            return res.status(501).json({ error:error.message });
+        }
+    },
+
+    login: async (req, res) => {
+        
     }
 
 };
--- APPointment/routes/index.js
+++ APPointment/routes/index.js
@@ -5,4 +5,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', controller.index);
 
+router.post('/register', controller.register);
+
 module.exports = router;
