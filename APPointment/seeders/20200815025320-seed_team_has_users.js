8bd8d6a54bdcdb4ef8233ead5d154ef0a478cb1a
Alessandro <alessandroadm@live.com>
2020-08-15 00:19:52 -0300

    feat(seeder): adding team seeder

--- APPointment/seeders/20200815025320-seed_team_has_users.js
+++ APPointment/seeders/20200815025320-seed_team_has_users.js
@@ -1,24 +1,34 @@
+const moment = require('moment');
+const currentDate = new Date();
+const faker = require('faker');
+
 'use strict';
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('team_has_users', [
+      {
+        'teams_id': 1,
+        'users_id': 1,
+        'createdat': moment(currentDate).subtract(1, 'day').format('YYYY-MM-DD hh:mm:ss'),
+        'updatedat': moment(currentDate).subtract(1, 'day').format('YYYY-MM-DD hh:mm:ss'),
+      },
+      {
+        'teams_id': 1,
+        'users_id': 2,
+        'createdat': moment(currentDate).subtract(1, 'day').format('YYYY-MM-DD hh:mm:ss'),
+        'updatedat': moment(currentDate).subtract(1, 'day').format('YYYY-MM-DD hh:mm:ss'),
+      },
+      {
+        'teams_id': 1,
+        'users_id': 3,
+        'createdat': moment(currentDate).subtract(1, 'day').format('YYYY-MM-DD hh:mm:ss'),
+        'updatedat': moment(currentDate).subtract(1, 'day').format('YYYY-MM-DD hh:mm:ss'),
+      }
+    ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+     await queryInterface.bulkDelete('team_has_users', null, {});
   }
 };
