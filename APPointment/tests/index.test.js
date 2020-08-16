2ea856ba222520fec59ca2cb4b8f9646ce6d56db
Alessandro <alessandroadm@live.com>
2020-08-16 17:13:38 -0300

    test(index): adding test to login

--- APPointment/tests/index.test.js
+++ APPointment/tests/index.test.js
@@ -3,12 +3,42 @@ const app = require('../app');
 const supertest = require('supertest');
 const request = supertest(app);
 
+let TOKEN = '';
+
+beforeAll(async () => {
+    const response = await request.post('/api/login')
+    .send({
+        email: 'admin@admin.com',
+        password:'admin'
+      });
+    TOKEN = response.body.token;
+});
+
 afterAll(async () => await User.sequelize.close());
 
-test('O status da resposta deve ser 200 ', async () => {
-    const response = await request.get('/api/');
+test('O status da resposta deve ser 422 pedir email e senha ', async () => {
+    const response = await request.post('/api/login');
     const status = response.status;
-    expect(status).toBe(200);
+    expect(status).toBe(422);
+});
+
+test('O status da resposta deve ser 422 pedir senha ', async () => {
+    const response = await request.post('/api/login')
+        .send({
+            email:'admin@admin.com'
+        });
+    const status = response.status;
+    expect(status).toBe(422);
+});
+
+test('O status da resposta deve ser 401 senha inválida ', async () => {
+    const response = await request.post('/api/login')
+        .send({
+            email:'admin@admin.com',
+            password:'123456'
+        });
+    const status = response.status;
+    expect(status).toBe(401);
 });
 
 test('O primeiro usuário deve ter id 1 ', async () => {
