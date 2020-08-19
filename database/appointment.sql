b28c76f6478b453756b134dbd58df2bf265b4de7
Alessandro <alessandroadm@live.com>
2020-08-19 20:27:12 -0300

    doc(database): adding script to create table on database

--- database/appointment.sql
+++ database/appointment.sql
@@ -1,60 +1,79 @@
+
 -- -----------------------------------------------------
 -- Table `users`
 -- -----------------------------------------------------
-CREATE TABLE users (
-  id SERIAL NOT NULL PRIMARY KEY,
-  name VARCHAR(80) NULL,
-  email VARCHAR(120) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  admin BOOLEAN NOT NULL DEFAULT false,
-  createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+
+CREATE TABLE public.users(
+    id SERIAL NOT NULL PRIMARY KEY,
+    name character varying(255) COLLATE pg_catalog."default",
+    email character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    admin boolean NOT NULL DEFAULT false,
+    created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+
+
+
+-- -----------------------------------------------------
+-- Table `teams`
+-- -----------------------------------------------------
+
+CREATE TABLE public.teams(
+    id SERIAL NOT NULL PRIMARY KEY,
+    description character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    manager integer NOT NULL,
+    CONSTRAINT teams_manager_fkey FOREIGN KEY (manager)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
 
 
 -- -----------------------------------------------------
 -- Table `tasks`
 -- -----------------------------------------------------
-CREATE TABLE tasks (
-  id SERIAL NOT NULL PRIMARY KEY,
-  startAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  doneAt TIMESTAMP NULL,
-  createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  description VARCHAR(120) NOT NULL,
-  done BOOLEAN NOT NULL DEFAULT false,
-  users_id INT NOT NULL REFERENCES users (id) 
+
+CREATE TABLE public.tasks
+(
+    id SERIAL NOT NULL PRIMARY KEY,
+    start_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    done_at timestamp with time zone,
+    created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    description character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    done boolean NOT NULL DEFAULT false,
+    users_id integer NOT NULL,
+    CONSTRAINT tasks_users_id_fkey FOREIGN KEY (users_id)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
--- -----------------------------------------------------
--- Table `teams`
--- -----------------------------------------------------
-CREATE TABLE teams (
-  id SERIAL NOT NULL PRIMARY KEY,
-  description VARCHAR(80) NOT NULL,
-  createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  manager INT NOT NULL  REFERENCES users (id)
-);
+
 
 -- -----------------------------------------------------
 -- Table `team_has_users`
 -- -----------------------------------------------------
 
-CREATE TABLE team_has_users (
-  teams_id INT NOT NULL,
-  users_id INT NOT NULL,
-  createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT fk_teams_has_users_teams1
-    FOREIGN KEY (teams_id)
-    REFERENCES teams (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_teams_has_users_users1
-    FOREIGN KEY (users_id)
-    REFERENCES users (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
+CREATE TABLE public.team_has_users
+(
+    id SERIAL NOT NULL PRIMARY KEY,
+    teams_id integer NOT NULL,
+    users_id integer NOT NULL,
+    created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT team_has_users_teams_id_fkey FOREIGN KEY (teams_id)
+        REFERENCES public.teams (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT team_has_users_users_id_fkey FOREIGN KEY (users_id)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 	
