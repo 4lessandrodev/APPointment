2207b3b8187261421c1c06da48079b2d975f1aba
Alessandro <alessandroadm@live.com>
2020-08-19 14:37:16 -0300

    test(index): register

--- APPointment/tests/index.test.js
+++ APPointment/tests/index.test.js
@@ -16,74 +16,78 @@ beforeAll(async () => {
 
 afterAll(async () => await User.sequelize.close());
 
-test('O status da resposta deve ser 422 pedir email e senha ', async () => {
-    const response = await request.post('/api/login');
-    const status = response.status;
-    expect(status).toBe(422);
-});
 
-test('O status da resposta deve ser 422 pedir senha ', async () => {
-    const response = await request.post('/api/login')
-    .send({
-        email:'admin@admin.com'
-    });
-    const status = response.status;
-    expect(status).toBe(422);
-});
-
-test('O status da resposta deve ser 401 senha inválida ', async () => {
-    const response = await request.post('/api/login')
-    .send({
-        email:'admin@admin.com',
-        password:'123456'
-    });
-    const status = response.status;
-    expect(status).toBe(401);
-});
-
-test('Usuário sem token não pode ter acesso as tarefas ', async () => {
-    const response = await request.get('/api/tasks');
-    const status = response.status;
-    expect(status).toBe(401);
-});
-
-test('Usuário sem token não pode ter acesso a usuários ', async () => {
-    const response = await request.get('/api/usuarios');
-    const status = response.status;
-    expect(status).toBe(401);
-});
-
-test('Deve pedir um email e uma senha para se cadastrar ', async () => {
-    const response = await request.post('/api/register');
-    const status = response.status;
-    expect(status).toBe(422);
-});
-
-test('Deve pedir uma senha para se cadastrar ', async () => {
-    const response = await request.post('/api/register')
-    .send({ email: faker.internet.email() });
-    const status = response.status;
-    expect(status).toBe(422);
-});
-
-test('Deve pedir um email para se cadastrar ', async () => {
-    const response = await request.post('/api/register')
-    .send({ password: faker.internet.password() });
+describe('Grupo de teste para a rota index', () => {
     
-    const status = response.status;
-    expect(status).toBe(422);
-});
-
-test('Deve cadastrar um novo usuário ', async () => {
-    const response = await request.post('/api/register')
+    test('O status da resposta deve ser 422 pedir email e senha ', async () => {
+        const response = await request.post('/api/login');
+        const status = response.status;
+        expect(status).toBe(422);
+    });
+    
+    test('O status da resposta deve ser 422 pedir senha ', async () => {
+        const response = await request.post('/api/login')
+        .send({
+            email:'admin@admin.com'
+        });
+        const status = response.status;
+        expect(status).toBe(422);
+    });
+    
+    test('O status da resposta deve ser 401 senha inválida ', async () => {
+        const response = await request.post('/api/login')
+        .send({
+            email:'admin@admin.com',
+            password:'123456'
+        });
+        const status = response.status;
+        expect(status).toBe(401);
+    });
+    
+    test('Usuário sem token não pode ter acesso as tarefas ', async () => {
+        const response = await request.get('/api/tasks');
+        const status = response.status;
+        expect(status).toBe(401);
+    });
+    
+    test('Usuário sem token não pode ter acesso a usuários ', async () => {
+        const response = await request.get('/api/usuarios');
+        const status = response.status;
+        expect(status).toBe(401);
+    });
+    
+    test('Deve pedir um email e uma senha para se cadastrar ', async () => {
+        const response = await request.post('/api/register');
+        const status = response.status;
+        expect(status).toBe(422);
+    });
+    
+    test('Deve pedir uma senha para se cadastrar ', async () => {
+        const response = await request.post('/api/register')
+        .send({ email: faker.internet.email() });
+        const status = response.status;
+        expect(status).toBe(422);
+    });
+    
+    test('Deve pedir um email para se cadastrar ', async () => {
+        const response = await request.post('/api/register')
+        .send({ password: faker.internet.password() });
+        
+        const status = response.status;
+        expect(status).toBe(422);
+    });
+    
+    test('Deve cadastrar um novo usuário ', async () => {
+        const response = await request.post('/api/register')
         .send({
             email: faker.internet.email(),
             password: faker.internet.password(),
         });
-    
-    const status = response.status;
-    expect(status).toBe(200);
-    
+        
+        const status = response.status;
+        expect(status).toBe(200);
+        
+    });
 });
 
 
